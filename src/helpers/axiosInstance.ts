1185b52e7e04241b236ba574a38c9f729a0ae6e3
esauMartinez <martinez117esau@gmail.com>
2024-05-07 11:39:18 -0600

    chenge url from project

--- src/helpers/axiosInstance.ts
+++ src/helpers/axiosInstance.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const instance = axios.create({
-	baseURL: '/timbrado',
+	baseURL: 'https://api-timbrado-pruebas-91ba3cef374b.herokuapp.com/api/v2',
 });
--- vite.config.ts
+++ vite.config.ts
@@ -6,12 +6,12 @@ export default defineConfig({
 	plugins: [vue()],
 	server: {
 		port: 3100,
-		proxy: {
-			'/timbrado': {
-				target: 'https://api-timbrado-pruebas-91ba3cef374b.herokuapp.com/api/v2',
-				changeOrigin: true,
-				rewrite: (path) => path.replace(/^\/timbrado/, ''),
-			},	
-		},
+		// proxy: {
+		// 	'/timbrado': {
+		// 		target: 'https://api-timbrado-pruebas-91ba3cef374b.herokuapp.com/api/v2',
+		// 		changeOrigin: true,
+		// 		rewrite: (path) => path.replace(/^\/timbrado/, ''),
+		// 	},	
+		// },
 	},
 });
