85c4bc67a1281fb533edbff7554525cf2e534bcd
esauMartinez <martinez117esau@gmail.com>
2024-05-10 12:47:22 -0600

    chage url api

--- src/helpers/axiosInstance.ts
+++ src/helpers/axiosInstance.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const instance = axios.create({
-	// baseURL: 'https://api-timbrado-pruebas-91ba3cef374b.herokuapp.com/api/v2',
-	baseURL: 'http://localhost:3200/api/v2',
+	baseURL: 'https://api-timbrado-pruebas-91ba3cef374b.herokuapp.com/api/v2',
+	// baseURL: 'http://localhost:3200/api/v2',
 });
