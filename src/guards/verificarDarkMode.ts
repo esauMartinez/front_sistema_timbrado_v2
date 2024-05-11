ecab0c1c678f3500e3657784d3ee83a0fad57179
esauMartinez <martinez117esau@gmail.com>
2024-05-11 11:31:32 -0600

    added verificarDarkMode file

---
+++ src/guards/verificarDarkMode.ts
@@ -0,0 +1,5 @@
+export const verificarDarkMode = (): boolean => {
+	const darkMode = localStorage.getItem('dark-mode');
+	const darkModeStorage: boolean = JSON.parse(darkMode);
+	return darkModeStorage;
+};
